bd30a89bceacc88a698c89a9836d9827490b778e
Pedro <pdfloresj@gmail.com>
2022-06-15 14:56:12 -0500

    Create react app

---
+++ article/src/AppTest.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+class AppTest extends Component {
+  render() {
+    return (
+      <div className="AppTest">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+
+export default AppTest;
